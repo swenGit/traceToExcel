cfd5771a68d855a1973d66e38e97170eba9f55c5
swenGit <wangshen411411@outlook.com>
2024-05-18 11:08:34 +0800

    增加单机拼接输出

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 
 import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.convert.Convert;
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.date.TimeInterval;
 import cn.hutool.core.io.FileUtil;
@@ -22,23 +23,25 @@ public class Main {
         final TimeInterval timer = DateUtil.timer();
 
         //输出excel 会自动创建
-        String path = "D:\\desktop\\01风场\\西六家子\\西六架子能管数据4-19\\西六架子能管数据4-19";
-        String outputFileName = path + "\\" + "writeTest3.xlsx";
+        String path = "D:\\desktop\\01风场\\一次调频\\鸡冠山\\2024-05-17";
+        String outputFileName = path + "\\" + "writeTest4.xlsx";
         //删除上次输出的文件
         FileUtil.del(outputFileName);
         //日志文件路径
-        String logFileName = "D:\\desktop\\01风场\\西六家子\\西六架子能管数据4-19\\西六架子能管数据4-19\\sanywind.trace.2024-04-19.{}.log";
+        String logFileName = "D:\\desktop\\01风场\\一次调频\\鸡冠山\\2024-05-17\\sanywind.trace.2024-05-17.{}.log";
         //默认UTF-8编码，可以在构造中传入第二个参数做为编码
 
         BigExcelWriter writer = ExcelUtil.getBigWriter(outputFileName,"turbineMeasurements");
 
+        String logNums = "27-28";
         //日志起始页
-        int startNumber = 18;
+        int startNumber = Convert.toInt(logNums.split("\\-")[0]);
         //日志结束页
-        int endNumber = 18;
+        int endNumber = Convert.toInt(logNums.split("\\-")[1]);
 
         ArrayList<String> turbineList = new ArrayList<>();
         ArrayList<String> gridReturnList = new ArrayList<>();
+        ArrayList<String> theoryPowerList = new ArrayList<>();
 
         // 筛选关键字
         for (int i = startNumber; i <= endNumber; i++) {
@@ -50,11 +53,14 @@ public class Main {
             strings1 = fileReader1.readLines();
             strings1.removeIf(s -> !s.contains("有功返回 gridReturnValues"));
             gridReturnList.addAll(strings1);
+            strings1 = fileReader1.readLines();
+            strings1.removeIf(s -> !s.contains("有功返回 theoryPower"));
+            theoryPowerList.addAll(strings1);
         }
-
+        int turbineNums = theoryPowerList.get(0).split("Power:")[1].split(",").length;
         // 单机数据
-        writeTurbines(turbineList, writer);
-        turbineList.clear();
+        writeTurbines(turbineList, theoryPowerList, writer);
+
         // 全场数据
         writer.setSheet("gridReturnValues");
         writeGridReturn(writer, gridReturnList);
@@ -63,13 +69,107 @@ public class Main {
         writer.setSheet("primaryFrequency");
         writePFC(writer, gridReturnList);
 
+        // 单机合并数据(有功功率, 功率参考点)
+        writer.setSheet("turbinesLong");
+        writeTurbinesLong(writer, turbineList, turbineNums);
+        writer.setSheet("turbinesLong2");
+        writeTurbinesLong2(writer, turbineList, turbineNums);
+
         //关闭writer，释放内存
+        turbineList.clear();
         gridReturnList.clear();
         writer.close();
         Console.log("用时:" + timer.interval() + "ms...");
     }
 
+    private static void writeTurbinesLong(BigExcelWriter writer, ArrayList<String> turbineList, int turbineSize) {
+        writer.setFreezePane(1);
+        List<List<Object>> rows = CollUtil.newArrayList();
+        // 准备表头
+//        String[] titleBase = {"有功功率", "功率参考点", "桨叶角度"};
+//        int[] titleIndexs = {7, 41, 12};
+//        double[] coeff = {1, 0.001, 1};
+        String[] titleBase = {"有功功率"};
+        int[] titleIndexs = {7};
+        double[] coeff = {1};
+        List<Object> titleList = new ArrayList<>();
+        titleList.add("时间");
+        for (int i = 0; i < turbineSize; i++) {
+            for (String base : titleBase) {
+                titleList.add(i + 1 + "-" + base);
+            }
+        }
+        rows.add(titleList);
+        // 准备数据
+        Console.log("准备单机长数据...");
+        for (int i = 0; i < turbineList.size(); i++) {
+            ArrayList<Object> info = CollUtil.newArrayList();
+            info.add(getTime(turbineList.get(i)));
+            String turbineInfo = turbineList.get(i).split("有功传入 turbineMeasurements:")[1];
+            JSONArray objects = new JSONArray(turbineInfo);
+            for (int turbIndex = 0; turbIndex < objects.size(); turbIndex++) {
+                JSONArray jsonArray = objects.getJSONArray(turbIndex);
+                List<Double> turbInfo = jsonArray.toList(Double.class);
+                for (int i1 = 0; i1 < titleIndexs.length; i1++) {
+                    int titleIndex = titleIndexs[i1];
+                    info.add(turbInfo.get(titleIndex) * coeff[i1]);
+                }
+            }
+            rows.add(info);
+        }
+
+        //一次性写出内容，强制输出标题
+        writer.write(rows);
+        rows = null;
+        Console.log("写入准备单机长数据成功...");
+
+    }
+
+    private static void writeTurbinesLong2(BigExcelWriter writer, ArrayList<String> turbineList, int turbineSize) {
+        writer.setFreezePane(1);
+        List<List<Object>> rows = CollUtil.newArrayList();
+        // 准备表头
+//        String[] titleBase = {"有功功率", "功率参考点", "桨叶角度"};
+//        int[] titleIndexs = {7, 41, 12};
+//        double[] coeff = {1, 0.001, 1};
+        String[] titleBase = {"状态机"};
+        int[] titleIndexs = {23};
+        double[] coeff = {1};
+        List<Object> titleList = new ArrayList<>();
+        titleList.add("时间");
+        for (int i = 0; i < turbineSize; i++) {
+            for (String base : titleBase) {
+                titleList.add(i + 1 + "-" + base);
+            }
+        }
+        rows.add(titleList);
+        // 准备数据
+        Console.log("准备单机长数据...");
+        for (int i = 0; i < turbineList.size(); i++) {
+            ArrayList<Object> info = CollUtil.newArrayList();
+            info.add(getTime(turbineList.get(i)));
+            String turbineInfo = turbineList.get(i).split("有功传入 turbineMeasurements:")[1];
+            JSONArray objects = new JSONArray(turbineInfo);
+            for (int turbIndex = 0; turbIndex < objects.size(); turbIndex++) {
+                JSONArray jsonArray = objects.getJSONArray(turbIndex);
+                List<Double> turbInfo = jsonArray.toList(Double.class);
+                for (int i1 = 0; i1 < titleIndexs.length; i1++) {
+                    int titleIndex = titleIndexs[i1];
+                    info.add(turbInfo.get(titleIndex) * coeff[i1]);
+                }
+            }
+            rows.add(info);
+        }
+
+        //一次性写出内容，强制输出标题
+        writer.write(rows);
+        rows = null;
+        Console.log("写入准备单机长数据成功...");
+
+    }
+
     private static void writePFC(BigExcelWriter writer, ArrayList<String> gridReturnList) {
+        writer.setFreezePane(1);
         Console.log("pfc working...");
         String title = "时间,实际下发指令,风场实发总有功,并网点有功反馈,平均线损反馈,一次调频指令反馈,一次调频使能,风场可用有功";
         int[] indexs = {37, 21, 46, 38, 39, 40, 15};
@@ -97,6 +197,7 @@ public class Main {
     }
 
     private static void writeGridReturn(BigExcelWriter writer, ArrayList<String> gridReturnList) {
+        writer.setFreezePane(1);
         Console.log("gridReturnList.size()");
         Console.log(gridReturnList.size());
         String GridReturntitle2 = "时间\t 标杆风机数量\t 风场风机故障数量\t 标杆风机并网数量\t 标杆风机总有功\t " +
@@ -129,30 +230,40 @@ public class Main {
         Console.log("写入全场数据成功...");
     }
 
-    private static void writeTurbines(ArrayList<String> turbineList, BigExcelWriter writer) {
+    private static void writeTurbines(ArrayList<String> turbineList, ArrayList<String> theoryPowerList, BigExcelWriter writer) {
+        writer.setFreezePane(1);
         Console.log("turbineList.size()");
         Console.log(turbineList.size());
         List<List<Object>> rows = CollUtil.newArrayList();
-        String turbineMeasurementsTitle = "时间\t风机\t风机正常\t维护\t能量管理平台停机指令\t通讯中断\t风机运行状态\t电网电压\t电网电流\t有功功率\t无功功率\t无功控制显示\t风速\t功率因数\t当前桨叶角度\t限功率百分比显示\t发电量\t发电机转速\t风向\t油温\t机舱温度\t室外温度\t轴1桨叶实际角度\t轴2桨叶实际角度\t限功率百分比\t算法状态机\t理论功率返回\t额定功率\t最小桨距角\t可利用号\t算法风向偏差大标志\t算法振动大标志\t低穿标志\t高穿标志\t无功降容\t净有功\tblank\tblank\tblank\tblank\tblank\tblank\t下调速度\t有功指令参考点\t上调速度";
+        String turbineMeasurementsTitle = "时间\t风机\t风机正常\t维护\t能量管理平台停机指令\t通讯中断\t风机运行状态\t电网电压" +
+                "\t电网电流\t有功功率\t无功功率\t无功控制显示\t风速\t功率因数\t当前桨叶角度\t限功率百分比显示\t发电量\t发电机转速" +
+                "\t风向\t油温\t机舱温度\t室外温度\t轴1桨叶实际角度\t轴2桨叶实际角度\t限功率百分比\t算法状态机\t理论功率返回\t额定功率" +
+                "\t最小桨距角\t可利用号\t算法风向偏差大标志\t算法振动大标志\t低穿标志" +
+                "\t高穿标志\t无功降容\t净有功\tblank\tblank\tblank\tblank\tblank\tblank\t下调速度\t有功指令参考点\t上调速度" +
+                "\t理论功率";
         final String[] split = turbineMeasurementsTitle.split("\t");
         rows.add(new ArrayList<>(Arrays.asList(split)));
-
-        for (String string : turbineList) {
-            String turbineMeasurements = string.split("有功传入 turbineMeasurements:")[1];
-            String time = getTime(string);
+        for (int j = 0; j < turbineList.size() - 1; j++) {
+            String turbineInfo = turbineList.get(j);
+            String theoryPowerArrStr = theoryPowerList.get(j);
+            String turbineMeasurements = turbineInfo.split("有功传入 turbineMeasurements:")[1];
+            String theoryPowers = theoryPowerArrStr.split("Power:")[1];
+            String time = getTime(turbineInfo);
             JSONArray objects = new JSONArray(turbineMeasurements);
+            JSONArray objects2 = new JSONArray(theoryPowers);
             for (int i = 0; i < objects.size(); i++) {
                 JSONArray jsonArray = objects.getJSONArray(i);
+                double theoryPower = Convert.toDouble(objects2.get(i));
                 List<Double> doubles = jsonArray.toList(Double.class);
                 ArrayList<Object> objects1 = new ArrayList<>();
 
                 objects1.add(time);
-                objects1.add(i+1);
+                objects1.add(i + 1);
                 objects1.addAll(doubles);
+                objects1.add(theoryPower);
                 rows.add(objects1);
             }
         }
-        turbineList.clear();
         Console.log("准备写入单机数据...");
 
         //一次性写出内容，强制输出标题
